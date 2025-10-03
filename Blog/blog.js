2baff0e2afeb040a2ceca6de98ed1f00586b08bf
ayanokoujikun230-wq <ayanokoujikun230@gmail.com>
2025-10-03 08:53:58 +0200

    tried js, i'm not an expert

--- Blog/blog.js
+++ Blog/blog.js
@@ -1,2 +1,4 @@
-const container = document.querySelector(".container");
-container.scrollLeft += 100;
+function openInNewTab(){
+    const url = "C:\Users\rchet\OneDrive\Documents\GitHub\Cipher-Syndicate\Blog\CVs\Curriculum_vitae_of_Reece_Chetty.pdf";
+    
+}
